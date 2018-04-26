ff9da6c4e9cf6438268efa91c7b1c19bb701fac5
ivvan18 <iatokarev@edu.hse.ru>
2018-04-27 02:05:29 +0300

    Import bug fixed(with deleted component) + firebase database improved - empty cities and cities without countries removed.

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,7 +5,6 @@ import {ChartSettingsModule} from '../chart-settings/chart-settings.module';
 import {ChartSectionModule} from '../chart-section/chart-section.module';
 import {DashboardService} from './dashboard.service';
 import {HttpServiceService} from './http-service.service';
-import {ChartWeatherPreviewComponent} from '../chart-weather-preview/chart-weather-preview.component';
 
 @NgModule({
   imports: [
